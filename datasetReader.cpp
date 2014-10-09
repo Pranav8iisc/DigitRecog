2d5aadb8464bb470c9e626823f7a8f82f5bd4b74
Pranav <pranav8iisc@gmail.com>
2014-10-09 19:18:18 +0530

    Added comment describing MNIST image and label data format

---
+++ datasetReader.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+-----------------------------
+-----------------------------
+MNIST Image data format:
+
+Magic number(4 bytes, Big endian)
+Number of images(4 bytes)
+Number of rows
+Number of columns
+Pixel
+Pixel
+..
+..
+Pixel
+------------------------------
+------------------------------
+
+------------------------------
+------------------------------
+MNIST Image label format:
+
+Magic number(4 bytes, Big endian)
+Number of items(4)
+Label
+Label
+..
+..
+Label
+-----------------------------
+-----------------------------
+*/
+
+
+
+
+
+void readTrainingSetImageFile()
+{
+	
+
+}
+
+void readTrainingSetLabelFile()
+{
+
+}
+
+void readTestSetLabelFile()
+{
+
+}
+
+void readTestSetImageFile()
+{
+
+}
+
