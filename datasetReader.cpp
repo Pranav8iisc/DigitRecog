e5746b04bedfbbc960a446e2f3c57672d3021238
Pranav <pranav8iisc@gmail.com>
2014-10-10 19:19:40 +0530

    modified training image data reader function

--- datasetReader.cpp
+++ datasetReader.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <opencv/highgui.h>
+
 
 using namespace std;
 
@@ -36,27 +38,46 @@ Label
 */
 
 
-
-
-
+// reads training image data
 void readTrainingSetImageFile()
 {
-	
+// file location : MNISTDataset/train-images.idx3-ubyte	
+	ifstream trainImageFile("MNISTDataset/train-images.idx3-ubyte", ios::binary);
+	char magicNumber[4];
+	for (unsigned i = 0; i < 4; i++)
+		trainImageFile.get(magicNumber[i]); 
 
+	char numberOfImages[4];
+	for (unsigned i = 0; i < 4; i++)
+		trainImageFile.get(numberOfImages[i]);
+	
+			
+	trainImageFile.close();
 }
 
+// reads training label data
 void readTrainingSetLabelFile()
 {
+// file location : MNISTDataset/train-label.idx3-ubyte
 
 }
 
-void readTestSetLabelFile()
-{
-
-}
-
+// reads test image data
 void readTestSetImageFile()
 {
+// file location : MNISTDataset/t10k-images.idx3-ubyte
 
 }
 
+// reads test label data
+void readTestSetLabelFile()
+{
+// file location : MNISTDataset/t10k-label.idx3-ubyte
+
+}
+
+// used only for Debugging purpose so as to be sure about the read training and testing image data.
+void saveImages()
+{
+
+}
