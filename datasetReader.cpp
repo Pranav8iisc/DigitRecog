62a48dfe6f5ce81f5a341bce0eef26347b3e0bfc
Pranav Kant Gaur <pranav8iisc@gmail.com>
2014-10-11 08:02:34 +0000

    (partial)code for reading dataset

--- datasetReader.cpp
+++ datasetReader.cpp
@@ -2,8 +2,15 @@
 #include <fstream>
 #include <opencv/highgui.h>
 
+#define MAX_FILENAME_CHAR 255
 
 using namespace std;
+
+unsigned nImages;
+unsigned char trainImageData;
+unsigned char trainLabelData;
+unsigned char testImageData;
+unsigned char testLabelData;
 
 /*
 -----------------------------
@@ -41,16 +48,26 @@ Label
 // reads training image data
 void readTrainingSetImageFile()
 {
-// file location : MNISTDataset/train-images.idx3-ubyte	
 	ifstream trainImageFile("MNISTDataset/train-images.idx3-ubyte", ios::binary);
+	
 	char magicNumber[4];
+	
 	for (unsigned i = 0; i < 4; i++)
 		trainImageFile.get(magicNumber[i]); 
 
-	char numberOfImages[4];
-	for (unsigned i = 0; i < 4; i++)
-		trainImageFile.get(numberOfImages[i]);
+	char nImages[4];
 	
+	for (unsigned i = 0; i < 4; i++)
+		trainImageFile.get(nImages[i]);
+	
+	unsigned nRows, nColumns, totalPixels;
+
+	totalPixels = nRows * nColumns;
+
+	trainImageData = new char[totalPixels];
+	
+	for (unsigned i = 0; i < totalPixels; i++)
+		trainImageFile.get(trainImageData[i]);
 			
 	trainImageFile.close();
 }
@@ -58,26 +75,97 @@ void readTrainingSetImageFile()
 // reads training label data
 void readTrainingSetLabelFile()
 {
-// file location : MNISTDataset/train-label.idx3-ubyte
+	ifstream trainLabelFile("MNISTDataset/train-label.idx3-ubyte", ios::binary);
+	
+	char magicNumber[4];
+	
+	for (unsigned i = 0; i < 4; i++)
+		trainLabelFile.get(magicNumber[i]); 
 
+	char nLabels[4];
+	
+	for (unsigned i = 0; i < 4; i++)
+		trainLabelFile.get(nLabels[i]);
+	
+	char labelData = new char[nLabels];
+	
+	for (unsigned i = 0; i < nLabels; i++)
+		trainLabelFile.get(labelData[i]);
+			
+	trainLabelFile.close();
 }
 
 // reads test image data
 void readTestSetImageFile()
 {
-// file location : MNISTDataset/t10k-images.idx3-ubyte
+	ifstream trainImageFile("MNISTDataset/t10k-images.idx3-ubyte", ios::binary);
+	
+	char magicNumber[4];
+	
+	for (unsigned i = 0; i < 4; i++)
+		testImageFile.get(magicNumber[i]); 
+
+	char nImages[4];
+	
+	for (unsigned i = 0; i < 4; i++)
+		testImageFile.get(nImages[i]);
+	
+	unsigned nRows, nColumns, totalPixels;
+
+	totalPixels = nRows * nColumns;
+
+	char imageData = new char[totalPixels];
+	
+	for (unsigned i = 0; i < totalPixels; i++)
+		testImageFile.get(imageData[i]);
+			
+	testImageFile.close();
 
 }
 
 // reads test label data
 void readTestSetLabelFile()
 {
-// file location : MNISTDataset/t10k-label.idx3-ubyte
+	ifstream trainLabelFile("MNISTDataset/t10k-label.idx3-ubyte", ios::binary);
+	
+	char magicNumber[4];
+	
+	for (unsigned i = 0; i < 4; i++)
+		testLabelFile.get(magicNumber[i]); 
+
+	char nLabels[4];
+	
+	for (unsigned i = 0; i < 4; i++)
+		testLabelFile.get(nLabels[i]);
+	
+	char labelData = new char[nLabels];
+	
+	for (unsigned i = 0; i < nLabels; i++)
+		testLabelFile.get(labelData[i]);
+			
+	testLabelFile.close();
 
 }
 
-// used only for Debugging purpose so as to be sure about the read training and testing image data.
+// for Debugging pupose
 void saveImages()
 {
+	// save training images
+	char fileName[MAX_FILENAME_CHAR];
+	
+	Iplimage opencvImage = cvCreateImage(cvSize(nRows, nColumns), IPL_DEPTH_8U, 1);
+	
+	for (unsigned imageId = 0; imageId < nImages; imageId++)
+	{
+		sprintf(fileName, "MNISTDataset/trainImages/%i.jpg", imageId);
+			for (unsigned i = 0; i < nPixels; i++)
+				opencvImage->ImageData[i] = trainImageData[imageId][i];
+	}
+}
 
+
+int main()
+{
+	cout << "Please enter number of images"; 
+	return 0;
 }
