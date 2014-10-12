1289d58065522f89dfe6bead9eef7015d7bb892b
Pranav Kant Gaur <pranav8iisc@gmail.com>
2014-10-12 13:43:08 +0000

    initial structure of neural network classes

--- neuralNetwork.h
+++ neuralNetwork.h
@@ -1,12 +1,32 @@
 /*
-Class defining generic structure of neural networks. This class may later be specialized to instantiate different types of networks.
+Classes defining generic structures for neural networks. These classes may later be specialized to instantiate different types of networks.
 */
 
+// defines neuron class
+template<class T>
 class neuron
 {
+	public:
+	
+		T *inputActivation;
+		T outputResponse;
+		neuron *forwardNeighborNeurons;
+		neuron *backwardNeighborNeurons;
+		
+		void computeActivation();	
 };
 
+// defines the generic neural network
 class neuralNetwork
 {
-
+	public:
+	
+		neuron *inputLayer;
+		neuron **hiddenLayer; // in general there may be arbitrary number of hidden layers
+		neuron *outputLayer;
+		
+		unsigned int nInputNeurons;
+		unsigned int* nHiddenNeurons;
+		unsigned int nOutputNeurons;
+		unsigned int nHiddenLayers;
 };
