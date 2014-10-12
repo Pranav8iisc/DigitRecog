da2c27eb6077cf7bfeb71fb5aada45e08b6e3e81
Pranav Kant Gaur <pranav8iisc@gmail.com>
2014-10-12 06:02:38 +0000

    added provision for specifying training method

--- mainApplication.cpp
+++ mainApplication.cpp
@@ -1,12 +1,14 @@
-#include <idxReader.h>
-#include <nnetwork.h>
-
+#include "idxReader.h"
+#include "nnetwork.h"
+#include "trainingAlgorithm.h"
 
 int main()
 {
 	IdxReader mnistTrainImageDataset, mnistTrainLabelDataset, mnistTestImageDataset, mnistTestLabelDataset;
 	
 	NNetwork nNetwork;
+	
+	TrainingAlgorithm trainingMethod;
 	
 	mnistTrainImageDataset.read("MNISTDataset/train-images.idx3-ubyte"); 
 	
@@ -16,7 +18,7 @@ int main()
 	
 	mnistTestLabelDataset.read("MNISTDataset/t10k-label.idx3-ubyte"); 
 	
-	nNetwork.train(mnistTrainImageDataset, mnistTrainLabelDataset); // reads parameters of type Dataset::IdxDataset, runs learning algorithm, saves estimated network parameters in a XML file
+	nNetwork.train(mnistTrainImageDataset, mnistTrainLabelDataset, trainingMethod); // reads parameters of type Dataset::IdxDataset, runs learning algorithm, saves estimated network parameters in a XML file
 	
 	nNetwork.predict(mnistTestImageDataset, mnistTestLabelDataset);	// reads parameters of type Dataset::IdxDataset, runs prediction procedure, saves estimated labels in a XML file
     
