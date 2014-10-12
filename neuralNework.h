2c599deba527211e1f680d997a874856c639b5b5
Pranav Kant Gaur <pranav8iisc@gmail.com>
2014-10-12 11:45:10 +0000

    initial commit

---
+++ neuralNework.h
@@ -0,0 +1,12 @@
+/*
+Class defining generic structure of neural networks. This class may later be specialized to instantiate different types of networks.
+*/
+
+class neuron
+{
+};
+
+class neuralNetwork
+{
+
+};
